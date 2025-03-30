28b350b35e2e49690fd2fb2cb12923668bd116a3
Nick Reuter <thenewimagineer@gmail.com>
2025-03-30 18:11:22 -0400

    Change social media logos based on theme

--- src/components/Logos/GitHubLink.tsx
+++ src/components/Logos/GitHubLink.tsx
@@ -1,13 +1,42 @@
+"use client";
+
 import Image from "next/image";
 import githubIconWhite from "../../../public/images/github/github-mark-white.svg";
+import githubIconDark from "../../../public/images/github/github-mark.svg";
 import Link from "next/link";
 import {LogoHeight, LogoWidth} from "@/components/Logos/LogoConstants";
+import { useEffect, useState } from "react";
 
 export default function GitHubLink() {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+        // Check initial color scheme
+        setIsDarkMode(window.matchMedia('(prefers-color-scheme: dark)').matches);
+        
+        // Listen for changes in color scheme
+        const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+        const handleChange = (e: MediaQueryListEvent) => {
+            setIsDarkMode(e.matches);
+        };
+        
+        mediaQuery.addEventListener('change', handleChange);
+        
+        // Cleanup
+        return () => {
+            mediaQuery.removeEventListener('change', handleChange);
+        };
+    }, []);
+
     return (
         <>
             <Link href="https://github.com/LowBudgetMan/">
-                <Image src={githubIconWhite} width={LogoWidth} height={LogoHeight} alt="Nick's GitHub profile"/>
+                <Image 
+                    src={isDarkMode ? githubIconWhite : githubIconDark} 
+                    width={LogoWidth} 
+                    height={LogoHeight} 
+                    alt="Nick's GitHub profile"
+                />
             </Link>
         </>
     )
--- src/components/Logos/LinkedInLink.tsx
+++ src/components/Logos/LinkedInLink.tsx
@@ -1,13 +1,42 @@
+"use client";
+
 import Image from "next/image";
 import linkedInIconWhite from "../../../public/images/linkedIn/In-White.svg";
+import linkedInIconBlue from "../../../public/images/linkedIn/In-Blue.svg";
 import Link from "next/link";
 import {LogoHeight, LogoWidth} from "@/components/Logos/LogoConstants";
+import { useEffect, useState } from "react";
 
 export default function LinkedInLink() {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+        // Check initial color scheme
+        setIsDarkMode(window.matchMedia('(prefers-color-scheme: dark)').matches);
+        
+        // Listen for changes in color scheme
+        const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+        const handleChange = (e: MediaQueryListEvent) => {
+            setIsDarkMode(e.matches);
+        };
+        
+        mediaQuery.addEventListener('change', handleChange);
+        
+        // Cleanup
+        return () => {
+            mediaQuery.removeEventListener('change', handleChange);
+        };
+    }, []);
+
     return (
         <>
             <Link href="https://www.linkedin.com/in/nick-reuter-79a0a7103/">
-                <Image src={linkedInIconWhite} width={LogoWidth} height={LogoHeight} alt="Nick's LinkedIn profile"/>
+                <Image 
+                    src={isDarkMode ? linkedInIconWhite : linkedInIconBlue} 
+                    width={LogoWidth} 
+                    height={LogoHeight} 
+                    alt="Nick's LinkedIn profile"
+                />
             </Link>
         </>
     )
